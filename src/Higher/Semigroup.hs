ce1b63d487d48d3387ef5b10b910bda08a200ba7
Rob Rix <rob.rix@me.com>
2018-03-11 10:49:49 -0400

    Define instances of Semigroup for Maybe and [].

--- src/Higher/Semigroup.hs
+++ src/Higher/Semigroup.hs
@@ -1,4 +1,13 @@
 module Higher.Semigroup where
 
+import Control.Applicative ((<|>))
+
 class Semigroup a where
   (<>) :: a x -> a x -> a x
+
+
+instance Semigroup Maybe where
+  (<>) = (<|>)
+
+instance Semigroup [] where
+  (<>) = (<|>)
