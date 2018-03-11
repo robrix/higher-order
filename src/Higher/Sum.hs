aadbcf8bd68749ccb4509f6c980e8036cce0add4
Rob Rix <rob.rix@me.com>
2018-03-11 11:32:13 -0400

    Give a default definition of +++.

--- src/Higher/Sum.hs
+++ src/Higher/Sum.hs
@@ -13,6 +13,7 @@ class Sum s where
 
   infixr 2 +++
   (+++) :: (l1 x -> l2 y) -> (r1 x -> r2 y) -> ((l1 `s` r1) x -> (l2 `s` r2) y)
+  f1 +++ f2 = inl . f1 ||| inr . f2
 
   infixr 2 |||
   (|||) :: (l x -> b) -> (r x -> b) -> ((l `s` r) x -> b)
@@ -21,7 +22,5 @@ instance Sum (:+:) where
   inl = L1
   inr = R1
 
-  (f1 +++ _)  (L1 a1) = L1 (f1 a1)
-  (_  +++ f2) (R1 a2) = R1 (f2 a2)
   (f1 ||| _)  (L1 a1) = f1 a1
   (_  ||| f2) (R1 a2) = f2 a2
