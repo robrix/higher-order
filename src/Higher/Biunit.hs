6cb35d923f49ef35165ba13c5270a10dfb8ee996
Rob Rix <rob.rix@me.com>
2018-03-11 11:51:49 -0400

    Move the unit bifunctor to its own module.

---
+++ src/Higher/Biunit.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable #-}
+module Higher.Biunit where
+
+import Data.Bifoldable
+import Data.Bifunctor
+import Data.Bitraversable
+
+data U2 a b = U2
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Bifoldable U2 where
+  bifoldMap _ _ _ = mempty
+
+instance Bifunctor U2 where
+  bimap _ _ _ = U2
+
+instance Bitraversable U2 where
+  bitraverse _ _ _ = pure U2
