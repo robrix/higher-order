a89f66d5b36e14019837e11604777ab5ca61f18c
Rob Rix <rob.rix@me.com>
2018-03-11 13:20:38 -0400

    Set the fixity & precedence of ~> & ~~>.

--- src/Higher/Function.hs
+++ src/Higher/Function.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE PolyKinds, RankNTypes, TypeOperators #-}
 module Higher.Function where
 
+infixr 0 ~>
 type a ~> b = forall x . a x -> b x
+
+infixr 0 ~~>
 type a ~~> b = forall x y . a x y -> b x y
