f830ea0846ccca10b51c7a78ebbabb5a4d073e8a
Rob Rix <rob.rix@me.com>
2018-03-11 11:27:21 -0400

    Define &&& and *** on higher products.

--- src/Higher/Product.hs
+++ src/Higher/Product.hs
@@ -11,6 +11,15 @@ class Product p where
   exl :: (l `p` r) ~> l
   exr :: (l `p` r) ~> r
 
+  infixr 3 ***
+  (***) :: (l1 x -> l2 y) -> (r1 x -> r2 y) -> ((l1 `p` r1) x -> (l2 `p` r2) y)
+
+  infixr 3 &&&
+  (&&&) :: (a -> l x) -> (a -> r x) -> (a -> (l `p` r) x)
+
 instance Product (:*:) where
   exl (l :*: _) = l
   exr (_ :*: r) = r
+
+  (f1 *** f2) (a1 :*: a2) = f1 a1 :*: f2 a2
+  f1 &&& f2 = (:*:) <$> f1 <*> f2
