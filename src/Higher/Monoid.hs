c6fd67bc88efbbf5a4f30944237070ec62177c19
Rob Rix <rob.rix@me.com>
2018-03-11 10:50:35 -0400

    Define Monoid instances for Maybe and [].

--- src/Higher/Monoid.hs
+++ src/Higher/Monoid.hs
@@ -1,6 +1,14 @@
 module Higher.Monoid where
 
 import Higher.Semigroup
+import Prelude hiding (Monoid)
 
 class Semigroup a => Monoid a where
   zero :: a x
+
+
+instance Monoid Maybe where
+  zero = Nothing
+
+instance Monoid [] where
+  zero = []
