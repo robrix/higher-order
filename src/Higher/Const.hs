a13e27b41524f5c76fe1109326304ca8fb6e3f6f
Rob Rix <rob.rix@me.com>
2018-03-11 13:22:51 -0400

    Define a Bifunctor instance for Const.

--- src/Higher/Const.hs
+++ src/Higher/Const.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, PolyKinds #-}
 module Higher.Const
 ( Const(..)
+, module H
 ) where
 
+import Higher.Bifunctor
 import Higher.Functor as H
 
 newtype Const a b x = Const { getConst :: a x }
@@ -10,3 +12,8 @@ newtype Const a b x = Const { getConst :: a x }
 
 instance H.Functor (Const a) where
   fmap _ (Const a) = Const a
+
+instance Bifunctor Const where
+  bimap f _ (Const a) = Const (f a)
+
+  second _ (Const a) = Const a
