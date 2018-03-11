e5219213129fc7be69a95e8132cbbe225f33c656
Rob Rix <rob.rix@me.com>
2018-03-11 11:47:53 -0400

    Rename the sum operators to avoid conflicts with Control.Arrow.

--- src/Higher/Sum.hs
+++ src/Higher/Sum.hs
@@ -11,16 +11,16 @@ class Sum s where
   inl :: l ~> (l `s` r)
   inr :: r ~> (l `s` r)
 
-  infixr 2 +++
-  (+++) :: (l1 x -> l2 y) -> (r1 x -> r2 y) -> ((l1 `s` r1) x -> (l2 `s` r2) y)
-  f1 +++ f2 = inl . f1 ||| inr . f2
+  infixr 2 <+++>
+  (<+++>) :: (l1 x -> l2 y) -> (r1 x -> r2 y) -> ((l1 `s` r1) x -> (l2 `s` r2) y)
+  f1 <+++> f2 = inl . f1 <|||> inr . f2
 
-  infixr 2 |||
-  (|||) :: (l x -> b) -> (r x -> b) -> ((l `s` r) x -> b)
+  infixr 2 <|||>
+  (<|||>) :: (l x -> b) -> (r x -> b) -> ((l `s` r) x -> b)
 
 instance Sum (:+:) where
   inl = L1
   inr = R1
 
-  (f1 ||| _)  (L1 a1) = f1 a1
-  (_  ||| f2) (R1 a2) = f2 a2
+  (f1 <|||> _)  (L1 a1) = f1 a1
+  (_  <|||> f2) (R1 a2) = f2 a2
