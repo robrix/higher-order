8b93b464231d6b832f409a936af5976f2201114e
Rob Rix <rob.rix@me.com>
2018-03-11 12:31:59 -0400

    Define extending operators.

--- src/Higher/Extend.hs
+++ src/Higher/Extend.hs
@@ -2,6 +2,8 @@
 module Higher.Extend
 ( Extend(..)
 , module H
+, (<<=)
+, (=>>)
 ) where
 
 import Higher.Function as H
@@ -9,3 +11,9 @@ import Higher.Function as H
 class Extend w where
   duplicate :: w a ~> w (w a)
   extend :: (w a ~> b) -> (w a ~> w b)
+
+(=>>) :: Extend w => w a x -> (w a ~> b) -> w b x
+w =>> f = extend f w
+
+(<<=) :: Extend w => (w a ~> b) -> (w a ~> w b)
+(<<=) = extend
