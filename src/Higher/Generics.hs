8db6d252902f9d068d46b8789b24e14cb73d6f0c
Rob Rix <rob.rix@me.com>
2018-03-11 11:00:06 -0400

    Define the constant bifunctor.

--- src/Higher/Generics.hs
+++ src/Higher/Generics.hs
@@ -4,6 +4,7 @@ module Higher.Generics where
 import Data.Bifoldable
 import Data.Bifunctor
 import Data.Bitraversable
+import Data.Coerce
 
 data U2 a b = U2
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
@@ -16,3 +17,16 @@ instance Bifunctor U2 where
 
 instance Bitraversable U2 where
   bitraverse _ _ _ = pure U2
+
+
+newtype K2 k a b = K2 { unK2 :: k }
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Bifoldable (K2 k) where
+  bifoldMap _ _ _ = mempty
+
+instance Bifunctor (K2 k) where
+  bimap _ _ = coerce
+
+instance Bitraversable (K2 k) where
+  bitraverse _ _ = pure . coerce
