f383d8a414f01f34d766a55dc3b093a17c563fbe
Rob Rix <rob.rix@me.com>
2018-03-11 21:11:16 -0400

    Define an applicative sequencing semigroup.

--- src/Higher/Semigroup.hs
+++ src/Higher/Semigroup.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Higher.Semigroup where
 
-import Control.Applicative ((<|>))
+import Control.Applicative (Alternative(..))
 
 class Semigroup a where
   (<>) :: a x -> a x -> a x
@@ -23,3 +24,10 @@ newtype Dual a x = Dual { getDual :: a x }
 
 instance Semigroup (Dual a) where
   (<>) = flip (<>)
+
+
+newtype App f a = App { getApp :: f a }
+  deriving (Alternative, Applicative, Eq, Foldable, Functor, Monad, Ord, Show, Traversable)
+
+instance Applicative f => Semigroup (App f) where
+  App a <> App b = App (a *> b)
